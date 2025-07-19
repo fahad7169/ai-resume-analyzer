22894ede974a0e7666d995bb4feaa017ee0f06fd
fahad7169 <fahadalilund62@gmail.com>
2025-07-19 18:49:19 +0500

    Implemented hero section

---
+++ app/routes/auth.tsx
@@ -0,0 +1,12 @@
+export const meta = ()=>([
+    {title:'Resumind | Auth'},
+    {name:'description',content:'Login into your account'},
+])
+
+const Auth = () => {
+  return (
+    <div>auth</div>
+  )
+}
+
+export default Auth
